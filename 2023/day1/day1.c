14c2c1d0497a218da46c919daf4edd929f81bb40
Patrick Lee <git@patricklee.nyc>
2023-12-01 11:29:30 -0500

    FINISHED DAY 1 WTF

--- 2023/day1/day1.c
+++ 2023/day1/day1.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -55,33 +56,65 @@ int part1() {
     return 0;
 }
 
-char getDigitSpelledOut(char *buffer) {
-    if (strstr(buffer, "one")) {
-        return '1';
-    } else if (strstr(buffer, "two")) {
-        return '2';
-    } else if (strstr(buffer, "three")) {
-        return '3';
-    } else if (strstr(buffer, "four")) {
-        return '4';
-    } else if (strstr(buffer, "five")) {
-        return '5';
-    } else if (strstr(buffer, "six")) {
-        return '6';
-    } else if (strstr(buffer, "seven")) {
-        return '7';
-    } else if (strstr(buffer, "eight")) {
-        return '8';
-    } else if (strstr(buffer, "nine")) {
-        return '9';
-    } else {
-        return '\0';
-    }
-}
+typedef enum {
+    SEARCH_FROM_START,
+    SEARCH_FROM_END
+} SearchDirection;
 
+char getDigitSpelledOut(const char *buffer, SearchDirection direction) {
+    const char *found = NULL;
+
+    if (direction == SEARCH_FROM_START) {
+        // Search from the start of the buffer
+        if ((found = strstr(buffer, "one")))
+            return '1';
+        else if ((found = strstr(buffer, "two")))
+            return '2';
+        else if ((found = strstr(buffer, "three")))
+            return '3';
+        else if ((found = strstr(buffer, "four")))
+            return '4';
+        else if ((found = strstr(buffer, "five")))
+            return '5';
+        else if ((found = strstr(buffer, "six")))
+            return '6';
+        else if ((found = strstr(buffer, "seven")))
+            return '7';
+        else if ((found = strstr(buffer, "eight")))
+            return '8';
+        else if ((found = strstr(buffer, "nine")))
+            return '9';
+    } else {
+        // Search from the end of the buffer
+        size_t len = strlen(buffer);
+        for (size_t i = len; i > 0; --i) {
+            const char *subBuffer = &buffer[i - 1];
+            if (strncmp(subBuffer, "one", 3) == 0)
+                return '1';
+            else if (strncmp(subBuffer, "two", 3) == 0)
+                return '2';
+            else if (strncmp(subBuffer, "three", 5) == 0)
+                return '3';
+            else if (strncmp(subBuffer, "four", 4) == 0)
+                return '4';
+            else if (strncmp(subBuffer, "five", 4) == 0)
+                return '5';
+            else if (strncmp(subBuffer, "six", 3) == 0)
+                return '6';
+            else if (strncmp(subBuffer, "seven", 5) == 0)
+                return '7';
+            else if (strncmp(subBuffer, "eight", 5) == 0)
+                return '8';
+            else if (strncmp(subBuffer, "nine", 4) == 0)
+                return '9';
+        }
+    }
+
+    return '\0';  // No match found
+}
 int part2() {
     long fileLen;
-    char *buffer = readFile("day1/test.txt", &fileLen);
+    char *buffer = readFile("day1/data.txt", &fileLen);
 
     if (!buffer) {
         perror("Error reading file");
@@ -98,7 +131,7 @@ int part2() {
     int lineCount = 0;
     int i = 0;
 
-    char temp[30];
+    char temp[100];
     int len = 0;
     do {
         char ch = buffer[i];
@@ -142,20 +175,18 @@ int part2() {
                 printf("ERROR: Buffer overflow\n");
             }
             temp[len] = '\0';
-            char maybeDigit = getDigitSpelledOut(temp);
-            if (maybeDigit != '\0') {
-                if (values[0] != '\0') {
-                    values[1] = maybeDigit;
-                } else {
+            if (values[0] == '\0') {
+                char maybeDigit = getDigitSpelledOut(temp, SEARCH_FROM_START);
+                if (maybeDigit != '\0') {
                     values[0] = maybeDigit;
                 }
-                // We probably shouldn't be clearing the whole thing, need to handle twone
-                // Clear the temp buffer
-                memset(temp, 0, sizeof(temp));
-                len = 0;
+            } else {
+                char maybeDigit = getDigitSpelledOut(temp, SEARCH_FROM_END);
+                if (maybeDigit != '\0') {
+                    values[1] = maybeDigit;
+                }
             }
         }
-
     } while (buffer[i++] != '\0');
 
     printf("Sum: %d\n", sum);
